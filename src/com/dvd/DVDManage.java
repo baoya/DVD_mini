656eab98aa4b032150f6894e0569fb8a308a5fcb
baoya <957653748@qq.com>
2016-10-13 17:15:42 +0800

    第四次提交

--- src/com/dvd/DVDManage.java
+++ src/com/dvd/DVDManage.java
@@ -3,83 +3,84 @@ package com.dvd;
 import java.util.Date;
 
 public class DVDManage {
-	
-	//属性
-	DVD[] dvds = new DVD[1000]; //假设最多一千张碟片
-	//这个管理类负责对这个数组进行增删改查
-	
-	public DVDManage()
-	{
-		dvds[0] = new DVD("CD0001","泰坦尼克","爱情片");
-		dvds[1] = new DVD("CD0002","叶问","动作片");
-		dvds[2] = new DVD("CD0003","大话西游","喜剧片");
-		dvds[3] = new DVD("CD0004","满城尽带黄金甲","烂片");
+
+	// 属性
+	DVD[] dvds = new DVD[1000]; // 假设最多一千张碟片
+
+	// 这个管理类负责对这个数组进行增删改查
+
+	public DVDManage() {
+		dvds[0] = new DVD("CD0001", "泰坦尼克", "爱情片");
+		dvds[1] = new DVD("CD0002", "叶问", "动作片");
+		dvds[2] = new DVD("CD0003", "大话西游", "喜剧片");
+		dvds[3] = new DVD("CD0004", "满城尽带黄金甲", "烂片");
 	}
-	
-	public boolean addDVD(DVD dvd){
-		//首先检查DVD编号是否重复
-		if(searchId(dvd.id)){
+
+	public boolean addDVD(DVD dvd) {
+		// 首先检查DVD编号是否重复
+		if (searchId(dvd.id)) {
 			System.out.println("DVD编号重复!");
 			return false;
-		}
-		else{
-			for(int i=0;i<dvds.length;i++){
-				if(dvds[i]==null){
-					dvds[i]=dvd;
+		} else {
+			for (int i = 0; i < dvds.length; i++) {
+				if (dvds[i] == null) {
+					dvds[i] = dvd;
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	public boolean searchId(String id){
-		
-		for(int i=0;i<dvds.length;i++){
-			if(dvds[i]==null){
+
+	public boolean searchId(String id) {
+
+		for (int i = 0; i < dvds.length; i++) {
+			if (dvds[i] == null) {
 				return false;
 			}
-			if(dvds[i].id.equals(id)){
+			if (dvds[i].id.equals(id)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	public boolean deleteDVD(String id){
+
+	public boolean deleteDVD(String id) {
 		int pos = -1;
-		for(int i=0;i<dvds.length;i++){
-			if(dvds[i].id.equals(id)){
+		for (int i = 0; i < dvds.length; i++) {
+			if (dvds[i].id.equals(id)) {
 				pos = i;
-				for(;pos<dvds.length&&dvds[pos]!=null;pos++){
-					dvds[pos] = dvds[pos+1];
+				for (; pos < dvds.length && dvds[pos] != null; pos++) {
+					dvds[pos] = dvds[pos + 1];
 				}
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	//方法
-	public void querryAllDvd(){
-		for(int i=0;i<dvds.length;i++){
-			if(dvds[i] == null){
-				break;//对象为空结束查询
+
+	// 方法
+	public void querryAllDvd() {
+		for (int i = 0; i < dvds.length; i++) {
+			if (dvds[i] == null) {
+				break;// 对象为空结束查询
 			}
 			System.out.println(dvds[i].showInfo());
 		}
 	}
-	
-	public boolean lendDVD(String id){
-		for(int i=0;i<dvds.length;i++){
-			if(dvds[i]==null){
+
+	public boolean lendDVD(String id) {
+		for (int i = 0; i < dvds.length; i++) {
+			if (dvds[i] == null) {
 				System.out.println("该碟片不存在！");
-				break;//遍历到头了都没有找到你要的DVD碟片编号，没有这张碟片
+				break;// 遍历到头了都没有找到你要的DVD碟片编号，没有这张碟片
 			}
-			if(dvds[i].id.equals(id)){///找到了这张DVD碟片
-				if(dvds[i].status){
+			if (dvds[i].id.equals(id)) {// /找到了这张DVD碟片
+				if (dvds[i].status) {
 					System.out.println("该碟片已经被借出！");
 					return false;
 				}
-				
+
 				dvds[i].status = true;
 				dvds[i].lendTime = new Date();
 				return true;
@@ -87,18 +88,19 @@ public class DVDManage {
 		}
 		return false;
 	}
-	public boolean returnDVD(String id){
-		for(int i=0;i<dvds.length;i++){
-			if(dvds[i]==null){
+
+	public boolean returnDVD(String id) {
+		for (int i = 0; i < dvds.length; i++) {
+			if (dvds[i] == null) {
 				System.out.println("该碟片不存在！");
-				break;//遍历到头了都没有找到你要的DVD碟片编号，没有这张碟片
+				break;// 遍历到头了都没有找到你要的DVD碟片编号，没有这张碟片
 			}
-			if(dvds[i].id.equals(id)){///找到了这张DVD碟片
-				if(!dvds[i].status){
+			if (dvds[i].id.equals(id)) {// /找到了这张DVD碟片
+				if (!dvds[i].status) {
 					System.out.println("该碟片未被借出！");
 					return false;
 				}
-				
+
 				dvds[i].status = false;
 				dvds[i].returnTime = new Date();
 				return true;
@@ -106,10 +108,9 @@ public class DVDManage {
 		}
 		return false;
 	}
-	
-	//显示主菜单的方法
-	public void showMenu()
-	{
+
+	// 显示主菜单的方法
+	public void showMenu() {
 		System.out.println("**********迷你DVD管理系统************");
 		System.out.println("1.查询所有DVD资料");
 		System.out.println("2.新增DVD资料");
--- src/com/dvd/Text.java
+++ src/com/dvd/Text.java
@@ -3,16 +3,16 @@ package com.dvd;
 import java.util.Scanner;
 
 public class Text {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		Scanner in = new Scanner(System.in);
 		int choice;
-		
+
 		DVDManage dm = new DVDManage();
 		dm.showMenu();
 		choice = in.nextInt();
-		while(choice!=6){
-			switch(choice){
+		while (choice != 6) {
+			switch (choice) {
 			case 1:
 				System.out.println("您选择了查询DVD资料......");
 				dm.querryAllDvd();
@@ -28,12 +28,11 @@ public class Text {
 				name = in.next();
 				System.out.println("请输入碟片的类型：");
 				type = in.next();
-				DVD dvd = new DVD(id2,name,type);
-				if(dm.addDVD(dvd)){
-					
+				DVD dvd = new DVD(id2, name, type);
+				if (dm.addDVD(dvd)) {
+
 					System.out.println("添加DVD成功！");
-				}
-				else{
+				} else {
 					System.out.println("添加DVD失败！");
 				}
 				break;
@@ -42,10 +41,9 @@ public class Text {
 				String id3;
 				System.out.println("请输入你要删除的DVD编号：");
 				id3 = in.next();
-				if(dm.deleteDVD(id3)){
+				if (dm.deleteDVD(id3)) {
 					System.out.println("删除成功");
-				}
-				else{
+				} else {
 					System.out.println("删除失败!");
 				}
 				break;
@@ -53,30 +51,26 @@ public class Text {
 				System.out.println("您选择了借出DVD......");
 				System.out.println("请输入您要借出的DVD的编号：");
 				String id4 = in.next();
-				if(dm.lendDVD(id4))
-				{
+				if (dm.lendDVD(id4)) {
 					System.out.println("借出成功！");
-				}
-				else{
-				System.out.println("借出失败！");
+				} else {
+					System.out.println("借出失败！");
 				}
 				break;
 			case 5:
 				System.out.println("您选择了归还DVD......");
 				System.out.println("请输入您要归还的DVD的编号：");
 				String id5 = in.next();
-				if(dm.returnDVD(id5))
-				{
+				if (dm.returnDVD(id5)) {
 					System.out.println("归还成功！");
-				}
-				else{
-				System.out.println("归还失败！");
+				} else {
+					System.out.println("归还失败！");
 				}
 				break;
 			}
 			dm.showMenu();
 			choice = in.nextInt();
-			
+
 		}
 		System.out.println("谢谢您的使用！");
 	}
